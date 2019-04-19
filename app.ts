8d8c31d9bf15decbb2b738a6a58e891734d058cf
victorlee1998 <16520361@gm.uit.edu.vn>
2019-04-19 15:28:17 +0700

    Remove connect database, just express server

--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
 import {Request, Response} from 'express';
 import * as express from 'express';
-import * as bodyParser from 'body-parser'
-import * as logger from 'morgan'
-import {createConnection} from 'typeorm';
-import ormconfig from './ormconfig';
+import * as bodyParser from 'body-parser';
+import * as logger from 'morgan';
 
 export const app: express.Application = express();
 
@@ -11,13 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 
-// Create connection to database with config is database detail
-createConnection(ormconfig).then(() => {
-    app.get('*', (_req: Request, res: Response) =>
-        res.status(200).send({
-            message: 'Welcome to test typescript',
-        }),
-    );
-}).catch((e) => {
-    console.error(e);
-});
+app.get('*', (_req: Request, res: Response) =>
+    res.status(200).send({
+        message: 'Welcome to test typescript',
+    }),
+);
