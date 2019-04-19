42b323f7e7f302f50c180ea225dfc6f5a773591a
victorlee1998 <16520361@gm.uit.edu.vn>
2019-04-19 14:46:43 +0700

    Config typeorm for heroku postgres

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,12 +5,11 @@ dotenv.config();
 
 const postgresTypeOrmConfig: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE,
+    url: process.env.DATABASE_URL,
     synchronize: true,
+    extra: {
+        ssl: true,
+    },
     logging: false,
     entities: ['src/entity/**/*.ts'],
     migrations: ['src/migration/**/*.ts'],
