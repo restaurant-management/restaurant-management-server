85924ed63f63f56dc7648cd7ba84072ff724ff5c
victorlee1998 <16520361@gm.uit.edu.vn>
2019-05-03 23:46:03 +0700

    Complete role router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,13 @@
 import {Router} from 'express';
 import userRouter from './user.router';
 import dishRouter from './dish.router';
+import roleRouter from './role.router';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/dishes', dishRouter);
+router.use('/roles', roleRouter);
 
 // Default Get.
 router.get('*', (_req, res) => {
---
+++ src/services/role.service.ts
@@ -0,0 +1,67 @@
+import {getConnection} from 'typeorm';
+import {Permission} from '../entities/Permission';
+import {Role} from '../entities/Role';
+import stringToSlug from '../helpers/slugHandler';
+
+const create = async (_name: string, _slug?: string, _description?: string, _permissions?: string[]) => {
+    if(!_slug) _slug = stringToSlug(_name);
+
+    if (await Role.findOne({where: {slug: _slug}}))
+        throw new Error('Role existed.');
+
+    let newRole = new Role();
+    newRole.slug = stringToSlug(_slug);
+    newRole.name = _name;
+    newRole.description = _description;
+    newRole.permissions = _permissions;
+    return await newRole.save();
+};
+
+const update = async (_slug: string, _newSlug?: string, _name?: string, _description?: string) => {
+    if (await Role.findOne({where: {slug: _newSlug}}))
+        throw new Error('New role slug existed.');
+
+    let role = await Role.findOne({where: {slug: _slug}});
+    role.slug = _newSlug ? stringToSlug(_newSlug) : role.slug;
+    role.name = _name || role.name;
+    role.description = _description || role.description;
+
+    return await role.save();
+};
+
+const deleteRole = async (slug: string) => {
+    const result = await getConnection().createQueryBuilder()
+        .delete().from(Role)
+        .where('slug = :slug', {slug})
+        .execute();
+    if (result.affected < 1) throw new Error('Delete role failed.');
+};
+
+const findBySlug = async (slug: number) => {
+    let role = await Role.findOne({where: {slug}});
+    if (!role) throw new Error('Not found.');
+    return role;
+};
+
+const getAll = async () => {
+    return await Role.find();
+};
+
+const addPermission = async (slug: string, permission: string) => {
+    if (Object.keys(Permission).map(value => Permission[value]).indexOf(permission) < 0)
+        throw new Error('Permission not found.');
+
+    let role = await Role.findOne({where: {slug}});
+    if(!role.permissions) role.permissions = [];
+    role.permissions.push(permission);
+    return await role.save();
+};
+
+const removePermission = async (slug: string, permission: string) => {
+    let role = await Role.findOne({where: {slug}});
+    let index = role.permissions.indexOf(permission);
+    if (index > -1) role.permissions.splice(index, 1);
+    return await role.save();
+};
+
+export {create, getAll, deleteRole, findBySlug, update, addPermission, removePermission}
