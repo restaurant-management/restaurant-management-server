4cb7d3956fc3d1e79990102f60eca30124d52605
victorlee1998 <16520361@gm.uit.edu.vn>
2019-05-24 18:07:00 +0700

    Fix get all bill

--- src/routers/bill.router.ts
+++ src/routers/bill.router.ts
@@ -1,24 +1,90 @@
-import {Router} from 'express';
-import * as billController from '../controllers/bill.controller';
-import {Permission} from '../entities/Permission';
-import Authorize from '../helpers/authorize';
+import { Router } from "express";
+import * as billController from "../controllers/bill.controller";
+import { Permission } from "../entities/Permission";
+import Authorize from "../helpers/authorize";
 
 const router = Router();
 
-router.post('/', Authorize([Permission.CreateBill, Permission.BillManagement]), billController.create);
-router.post('/custom', Authorize(Permission.BillManagement), billController.createCustom);
-router.put('/:billId', Authorize(Permission.BillManagement), billController.editBill);
-router.put('/:billId/created', Authorize([Permission.BillManagement, Permission.UpdatePaidBillStatus]), billController.updateCreatedStatus);
-router.put('/:billId/paid', Authorize([Permission.BillManagement, Permission.UpdatePaidBillStatus]), billController.updatePaidStatus);
-router.put('/:billId/delivering', Authorize([Permission.BillManagement, Permission.UpdateDeliveringBillStatus]), billController.updateDeliveringStatus);
-router.put('/:billId/preparing', Authorize([Permission.BillManagement, Permission.UpdatePreparingBillStatus]), billController.updatePreparingStatus);
-router.put('/:billId/prepare-done', Authorize([Permission.BillManagement, Permission.UpdatePrepareDoneBillStatus]), billController.updatePrepareDoneStatus);
-router.put('/:billId/shipping', Authorize([Permission.BillManagement, Permission.UpdateShippingBillStatus]), billController.updateShippingStatus);
-router.put('/:billId/complete', Authorize([Permission.BillManagement, Permission.UpdateCompleteBillStatus]), billController.updateCompleteStatus);
-router.get('/', Authorize(Permission.BillManagement), billController.getAll);
-router.get('/:billId', Authorize(), billController.getById);
-router.delete('/:billId', Authorize(Permission.BillManagement), billController.deleteBill);
-router.post('/:billId/dishes/:dishId', Authorize(Permission.BillManagement), billController.addDish);
-router.delete('/:billId/dishes/:dishId', Authorize(Permission.BillManagement), billController.removeDish);
+router.post(
+  "/",
+  Authorize([Permission.CreateBill, Permission.BillManagement]),
+  billController.create
+);
+router.post(
+  "/custom",
+  Authorize(Permission.BillManagement),
+  billController.createCustom
+);
+router.put(
+  "/:billId",
+  Authorize(Permission.BillManagement),
+  billController.editBill
+);
+router.put(
+  "/:billId/created",
+  Authorize([Permission.BillManagement, Permission.UpdatePaidBillStatus]),
+  billController.updateCreatedStatus
+);
+router.put(
+  "/:billId/paid",
+  Authorize([Permission.BillManagement, Permission.UpdatePaidBillStatus]),
+  billController.updatePaidStatus
+);
+router.put(
+  "/:billId/delivering",
+  Authorize([Permission.BillManagement, Permission.UpdateDeliveringBillStatus]),
+  billController.updateDeliveringStatus
+);
+router.put(
+  "/:billId/preparing",
+  Authorize([Permission.BillManagement, Permission.UpdatePreparingBillStatus]),
+  billController.updatePreparingStatus
+);
+router.put(
+  "/:billId/prepare-done",
+  Authorize([
+    Permission.BillManagement,
+    Permission.UpdatePrepareDoneBillStatus
+  ]),
+  billController.updatePrepareDoneStatus
+);
+router.put(
+  "/:billId/shipping",
+  Authorize([Permission.BillManagement, Permission.UpdateShippingBillStatus]),
+  billController.updateShippingStatus
+);
+router.put(
+  "/:billId/complete",
+  Authorize([Permission.BillManagement, Permission.UpdateCompleteBillStatus]),
+  billController.updateCompleteStatus
+);
+router.get(
+  "/",
+  Authorize([
+    Permission.BillManagement,
+    Permission.UpdatePaidBillStatus,
+    Permission.UpdatePreparingBillStatus,
+    Permission.UpdatePrepareDoneBillStatus,
+    Permission.UpdateDeliveringBillStatus,
+    Permission.UpdateCompleteBillStatus,
+  ]),
+  billController.getAll
+);
+router.get("/:billId", Authorize(), billController.getById);
+router.delete(
+  "/:billId",
+  Authorize(Permission.BillManagement),
+  billController.deleteBill
+);
+router.post(
+  "/:billId/dishes/:dishId",
+  Authorize(Permission.BillManagement),
+  billController.addDish
+);
+router.delete(
+  "/:billId/dishes/:dishId",
+  Authorize(Permission.BillManagement),
+  billController.removeDish
+);
 
 export default router;
